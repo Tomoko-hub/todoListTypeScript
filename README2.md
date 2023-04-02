9aa5431f83676808b0f1b514685f23865487da96
Tomoko-hub <haisaitomoco@gmail.com>
2023-04-02 16:03:45 +0300

    create todo, and handleSubmit/handleChange function

---
+++ README2.md
@@ -0,0 +1,22 @@
+# TODO APP TypeScript
+
+1. create application : npx creat-react-app todolist-youtube --template typescript
+    node : v16.14.2
+
+    note to myself: 
+        onSubmit:フォームにタスクを打ち、エンターキーを押したときに、どういう操作をするのか指定
+        input:タスクを打ち込むところ。
+        onChange:inputに文字を打ち込む度に呼ぶプロパティ。
+        input type="submit"はボタンとしての機能も果たす。
+
+    todo: {
+        value: string,
+        id: number,
+        checked: boolean,
+    }
+    のプロパティを持つ
+
+    送信ボタン（Submit）:e.preventDefalut
+
+    map : 子要素に対してkeyの設定
+    
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue]=useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  }
+
+  const handleChange =(event: React.ChangeEvent<HTMLInputElement>)=>{
+    //console.log(event.target.value);
+    setInputValue(event.target.value);
+  }
+
+  const handleSubmit =(event: React.FormEvent<HTMLFormElement>)=>{
+    event.preventDefault();
+    //create Todos
+    const newTodo: Todo ={
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    };
+
+    setTodos([newTodo, ...todos]);
+    setInputValue("");
+  };
+
+  const handleEdit=()=>{
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>ToDoList with Type Script</h2>
+        <form onSubmit={(event)=>handleSubmit(event)}>
+          <input 
+            type="text"
+            onChange={(event)=>handleChange(event)}
+            className='inputText'
+          />
+          <input 
+            type="submit"
+            value="create"
+            className='submitButton'
+             />
+        </form>
+        <ul className='todoList'>
+          {todos.map((todo)=>(
+            <li key={todo.id}>
+              <input
+                type="text"
+                onChange={(event)=>handleEdit(event)}
+                className='inputText'
+               />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
